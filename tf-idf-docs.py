76e87aeee9bd4cadfd9200c79da4b0766ef164e8
tomlai19852004 <tomlaihosum@hotmail.com>
2016-04-12 12:26:29 +0800

    remove dynamodb

--- tf-idf-docs.py
+++ tf-idf-docs.py
@@ -1,16 +1,8 @@
 import pandas as pd
 import numpy as np
-import boto3
 import os
 import codecs
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-client = boto3.client(
-	'dynamodb',
-	aws_access_key_id="AKIAJFEHWFDAGPYSMRQQ",
-	aws_secret_access_key="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",
-	region_name="ap-northeast-1"
-)
 
 fields = ['architecture','artist','socialmedia','teacher']
 field = 'architecture'
@@ -52,28 +44,16 @@ def map_score_phrase(tfidf_vec, score_indices):
 		highest_scoring_features.append(feature_names[index])
 	return highest_scoring_features
 
-# def get_highest_scoring_feature(array, feature_name, nth):
-# 	results = []
-# 	for ind in range(0, len(array)):
-# 		highest_scores_indices = np.argpartition(array[ind], kth=nth)[nth:]
-# 		scores = []
-# 		names = []
-# 		for index in highest_scores_indices:
-# 			scores.append(array[ind][index])
-# 			names.append(feature_names[index])
-# 		results.append(zip(scores, names))
-# 	return results
-
 def get_highest_scoring_feature(array, feature_name, nth):
 	results = []
 	for ind in range(0, len(array)):
 		highest_scores_indices = np.argpartition(array[ind], kth=nth)[nth:]
-		score_item = []
+		scores = []
+		names = []
 		for index in highest_scores_indices:
-			score_item.append({'L':{'tfidf':{'S':array[ind][index]}, 'word':{'S':feature_names[index].encode('utf-8')}}})
-		results.append(score_item)
-	# print results
-	# print '\n\n'
+			scores.append(array[ind][index])
+			names.append(feature_names[index])
+		results.append(zip(scores, names))
 	return results
 		
 all_docs = []
@@ -102,18 +82,7 @@ try:
 		map_doc_with_feature.append(dict)
 	
 	print map_doc_with_feature
-	dataId = 0
-	for dy_item in map_doc_with_feature:
-		client.put_item(
-			TableName="DataScience",
-			Item={
-				'dataId': {'BS':dataId}, 
-				'Text':{'S':dy_item['Text']},
-				'Keywords':{'L':dy_item['Keywords']}
-			}
-		)
-		dataId += 1
-
+	
 	# print total_highest_scores	
 	# print tfidf_vec.get_stop_words()
 except ValueError:
